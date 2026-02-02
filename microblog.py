6129c2ae4e2c8163ea208f5837a0b5dc782c63f2
kentjoshuanesus <kentjoshuanesus.it@gmai.com>
2026-02-02 17:15:25 +0800

    second commit

--- microblog.py
+++ microblog.py
@@ -1,8 +1,3 @@
-import sqlalchemy as sa
-import sqlalchemy.orm as so
-from app import app, db
-from app.models import User, Post
+from app import create_app, db
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+app = create_app()
